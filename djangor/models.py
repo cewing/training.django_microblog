8091383000aaf96b1e00c3c115c05b2fd23ecf5a
cewing <cris@crisewing.com>
2013-02-17 15:39:49 -0800

    * include our app in settings to install  * add urls for app to project urls  * add model for Entry class

--- djangor/models.py
+++ djangor/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    author = models.ForeignKey(User)
+    pub_date = models.DateTimeField()
+
+    class Meta:
+        ordering = ['-pub_date', ]
+        verbose_name_plural = "entries"
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self):
+        if not self.pk:
+            # this object is new, set pub_date
+            self.pub_date = timezone.now()
+        super(Entry, self).save()
